3209001efa43a4a3fc2b3920ff754a633da138c1
Sandeep Kharbanda <Sankharbanda@gmail.com>
2026-07-29 10:08:31 +0530

    feat(core): create enterprise service registry

---
+++ src/core/services/index.ts
@@ -0,0 +1,6 @@
+export { default as UserService } from "./UserService";
+export { default as RoleService } from "./RoleService";
+export { default as OrganisationService } from "./OrganisationService";
+export { default as PermissionService } from "./PermissionService";
+export { default as AuditService } from "./AuditService";
+export { default as NotificationService } from "./NotificationService";
